582a9cb9d3a59b5d69b8d7c1cb7a93c09faadc3d
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-04 22:44:57 -0700

    Add games index route and template

--- controllers/games.js
+++ controllers/games.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
+const Game = require('../models/game');
 
-
+router.get('/', async (req, res) => {
+    try {
+        const games = await Game.find();
+        res.render('games/index.ejs', {
+            games,
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
 
 module.exports = router;
