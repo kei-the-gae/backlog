36c998a2aa38373508b6c29c6f698ca71679174b
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-04 23:18:40 -0700

    Add user index route and template, add link to navbar

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 const User = require('../models/user');
 
+router.get('/', async (req, res) => {
+    try {
+        const allUsers = await User.find();
+        res.render('users/index.ejs', {
+            users: allUsers,
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
 router.get('/:userId', async (req, res) => {
     try {
         const targetUser = await User.findById(req.params.userId);
