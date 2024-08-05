90caaf06b88578081e9b222c7a2e7eb8a52f139b
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-04 21:36:06 -0700

    Add user show route and template, update link in navbar to access current user's page

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-
+router.get('/:userId', async (req, res) => {
+    try {
+        const targetUser = await User.findById(req.params.userId);
+        res.render('users/show.ejs', {
+            displayName: targetUser.displayName,
+            username: targetUser.username,
+        });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
 
 module.exports = router;
